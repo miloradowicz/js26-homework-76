93dd8048a376225499674104304c07a74fd04572
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-12-07 23:28:02 +0600

    messages api complete

---
+++ api/routers/messages.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import fileDb from '../fileDb';
+import { MessageBody } from '../types';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const datetime = req.query.datetime as string;
+
+  if (datetime === undefined) {
+    res.send(fileDb.getItems().slice(-30));
+  } else if (!Number.isNaN(Date.parse(datetime))) {
+    res.send(
+      fileDb
+        .getItems()
+        .filter((x) => Date.parse(x.datetime) > Date.parse(datetime))
+    );
+  } else {
+    res.status(400).send();
+  }
+});
+
+router.post('/', async (req, res) => {
+  const body = req.body as MessageBody;
+
+  if (body.author || body.message) {
+    res.status(400).send();
+  }
+
+  const message = {
+    id: crypto.randomUUID(),
+    author: body.author,
+    message: body.message,
+    datetime: new Date().toISOString(),
+  };
+
+  await fileDb.addItem(message);
+
+  res.send(message);
+});
+
+export default router;
